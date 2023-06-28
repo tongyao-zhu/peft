3b47ee7ced6738dba7634624d9fe61612ba06813
Tongyao Zhu <41590635+tongyao-zhu@users.noreply.github.com>
2023-06-28 17:51:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "packaging>=20.0",
         "psutil",
         "pyyaml",
-        "torch>=1.13.0",
+       #  "torch>=1.13.0", removed to ensure successful installation on AIP
         "transformers",
         "accelerate",
         "safetensors",
